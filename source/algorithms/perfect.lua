8039ef168c2f8b81b861a601df5b2fba347b0a22
Shironecko <shironecko.mail@gmail.com>
2014-07-26 03:12:36 +0300

    Implemented Prim's algorithm.

--- source/algorithms/perfect.lua
+++ source/algorithms/perfect.lua
@@ -44,3 +44,53 @@ function Maze:Backtracker(maze)
   
   maze:resetVisited()
 end
+
+-- Prim's algorithm
+function Maze:Prim(maze)
+  maze:resetDoors(true)
+  
+  local frontiers = {}
+  local cell = { x = math.random(#maze[1]), y = math.random(#maze) }
+  
+  while true do
+    maze[cell.y][cell.x].visited = true
+    maze[cell.y][cell.x].frontier = nil
+    
+    for key, value in pairs(self.directions) do
+      local newPos = { x = cell.x + value.x, y = cell.y + value.y }
+      
+      -- Marking every adjastment cell as a frontier, if not done so already
+      if maze[newPos.y] and maze[newPos.y][newPos.x] and not maze[newPos.y][newPos.x].visited and
+      not maze[newPos.y][newPos.x].frontier then
+        print("yeah")
+        maze[newPos.y][newPos.x].frontier = true
+        frontiers[#frontiers + 1] = newPos
+      end
+    end
+    
+    -- If there are no frontiers left - our job here is done
+    if #frontiers == 0 then break end
+    
+    -- Choosing random frontier
+    local rand_i = math.random(#frontiers)
+    local rand_f = frontiers[rand_i]
+    -- Removing it from the list
+    frontiers[rand_i] = frontiers[#frontiers]
+    frontiers[#frontiers] = nil
+    
+    -- Choosing random 'in' adjastment cell to carve from
+    local ins = {}
+    for key, value in pairs(self.directions) do
+      local newPos = { x = rand_f.x + value.x, y = rand_f.y + value.y }
+      
+      if maze[newPos.y] and maze[newPos.y][newPos.x] and maze[newPos.y][newPos.x].visited then
+        ins[#ins + 1] = key
+      end
+    end
+    
+    maze[rand_f.y][rand_f.x][ins[math.random(#ins)]]:open()
+    cell = rand_f
+  end
+    
+  maze:resetVisited()
+end

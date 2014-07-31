80771f3260d11145fd197fde9bb2065cc8b5e1a2
Shironecko <shironecko.mail@gmail.com>
2014-07-31 18:37:50 +0300

    Removed old testing file.

--- source/testing.lua
+++
@@ -1,9 +0,0 @@
-require "maze"
-require "algorithms/perfect"
-
-maze = Maze:Create(10, 10, true)
---[[  Maze generation depends on the random seed, so you will get 
-      exactly identical maze every time you pass exactly identical seed ]]
-math.randomseed(os.time())
-Maze:Prim(maze)
-print(maze:tostring())

1131aa015c8b74d9405361d4be61c1639f5f9564
Shironecko <shironecko.mail@gmail.com>
2014-10-27 04:38:03 +0200

    Sidewinder reformat

--- source/maze/generators/sidewinder.lua
+++ source/maze/generators/sidewinder.lua
@@ -8,22 +8,28 @@ local function sidewinder(maze)
   maze:ResetDoors(true)
   
   local set = {}
-  for y = 1, #maze - 1 do
-    for x = 1, #maze[1] do
+  for y = 1, maze:height() - 1 do
+    -- for each cell in a row
+    for x = 1, maze:width() do
+      -- add cell to the set
       set[#set + 1] = { x = x, y = y }
       
-      if random(2) == 1 and x ~= #maze[1] then
+      -- randomly choose will we carve east (right)
+      if random(2) == 1 and x ~= maze:width() then
         maze[y][x].east:Open()
       else
+        -- if not, carve north (down) from the random cell in the current set
         local cell = set[random(#set)]
         maze[cell.y][cell.x].south:Open()
+        -- start a new set
         set = {}
       end
     end
   end
   
-  for x = 1, #maze[1] - 1 do
-    maze[#maze][x].east:Open()
+  -- open all side walls of the last row
+  for x = 1, maze:width() - 1 do
+    maze[maze:height()][x].east:Open()
   end
 end
 
